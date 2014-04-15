dc9db8558901cf76917d4ed65306e41c1772bd36
Daniel Wolff <daniel@rapido.net.br>
2014-04-15 16:28:45 -0300

    implemented service sPeople

--- app/scripts/directives/uPersonSearch.js
+++ app/scripts/directives/uPersonSearch.js
@@ -3,11 +3,11 @@
 angular.module('frontdeskApp')
   .controller('uPersonSearchCtrl', function($scope, $http) {
     $scope.doSearch = function() {
-      $http.get('scripts/test.json').then(function(ret) {
-        // FIXME: this is getting people directly from person controller's
-        // scope; it would be better to have a service holding it, since
-        // it's going to be used by multiple directives
-        $scope.people = ret.data.results;
+      var strSearch = $scope.strSearch;
+      return sPeople(strSearch, $http).then(function(result) {
+        $scope.people = result.results;
+        $scope.results_number = result.results_number;
+        $scope.show_results = true;
       });
     };
   })
