db8cbb4159035148c7f84923260df5fbf0bcd836
Felipe Mobus <fmobus@gmail.com>
2014-04-26 15:55:25 -0300

    it should search for the nicest name straight away ;)

--- app/modules/controllers/search.js
+++ app/modules/controllers/search.js
@@ -3,13 +3,15 @@
 angular
   .module('fd.search',['ui.router', 'ngResource'])
   .controller('SearchCtrl', function($scope, $resource) {
-    $scope.query = '';
+    $scope.query = 'Felipe';
     $scope.results = [];
 
     $scope.search = function() {
       console.log($scope);
       $scope.results = $resource('http://localhost:3000/search/'+$scope.query).query();
     };
+    
+    $scope.search();
   })
   .config(function($stateProvider) {
     $stateProvider
