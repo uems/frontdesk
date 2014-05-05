85bd7a0038449a66db3ff72f1bbc43bd50538058
Felipe Mobus <fmobus@gmail.com>
2014-05-05 01:42:34 -0300

    debug messages on lazyCommit

--- app/modules/factories/lazyCommit.js
+++ app/modules/factories/lazyCommit.js
@@ -5,11 +5,13 @@ angular
   .factory('lazyCommit', function() {
     return function(commitFn, locator, nextState, oldEntry, scope, field) {
       return function() {
+        console.log(scope.step[field], oldEntry[field]);
         if (scope.step[field] === oldEntry[field]) {
           console.log('no changes for field '+field+', so I will be lazy');
           scope.fastForward(nextState);
         }
         else {
+          console.log('commiting changes on ', field);
           commitFn(locator, scope.step, function() { scope.reload(nextState); });
         }
       };
